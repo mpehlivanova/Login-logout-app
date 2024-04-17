edd1115ee0970e5b8744d347559410f27d258b87
Mariela Pehlivanova <mpehlivanova91@gmail.com>
2024-04-17 15:01:11 +0300

    feat: added restrict access component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,23 +3,25 @@ import { Link } from 'react-router-dom';
 import { Grid } from '@mui/material';
 import { useUserContext } from '../hooks/useUserContext';
 import { routeConfiguration } from '../layout/constants';
+import { RestrictedAccess } from '../auth/restricted-access'
 import { RouteConfigurationType } from '../types';
 import { Pages } from '../enum';
 
 const HomePage = () => {
   const { user } = useUserContext();
+  const permissionsUser: any = user?.permissions;
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}><h1>Welcome, {user?.name || user?.email} !</h1></Grid>
       <Grid item container spacing={2} flexDirection="row">
         {Object.values(routeConfiguration).map(({ isRequireAuth, url, id, permission }: RouteConfigurationType) => {
           if (isRequireAuth && id !== Pages.home) {
-            const hasAllPermission: boolean | undefined = permission?.reduce((acc: boolean, current: string) => {
-              return acc && Boolean(user?.permissions?.includes(current));
-            }, true);
-            if (hasAllPermission) {
-              return <Grid item key={id}><Link to={`${url}`}>{id} page</Link></Grid>
-            }
+            return (
+              <RestrictedAccess permissionsPage={permission} permissionsUser={permissionsUser}>
+                <Grid item key={id}><Link to={`${url}`}>{id} page</Link></Grid>
+              </RestrictedAccess>
+            )
           }
         })}
       </Grid>
