7eab1681d9a71bb0d145df77feb73d4c9f75cff5
Mariela Pehlivanova <mpehlivanova91@gmail.com>
2024-04-17 15:01:11 +0300

    feat: added restrict access component

---
+++ src/auth/restricted-access.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { PERMISSIONS } from '../enum'
+
+export const RestrictedAccess: React.FC<{ children: JSX.Element, permissionsPage?: PERMISSIONS[], permissionsUser: PERMISSIONS[] }> = ({
+  permissionsPage, permissionsUser, children
+}) => {
+  const hasPermission: boolean | undefined = permissionsPage?.reduce((acc: boolean, current: PERMISSIONS) => {
+    return acc && Boolean(permissionsUser?.includes(current));
+  }, true);
+  if (hasPermission) {
+    return children;
+  }
+  return null;
+};
