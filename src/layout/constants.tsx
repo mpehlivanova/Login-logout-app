7ac0fc9f780577c00ddc7b27be7ee7ab2873acef
Mariela Pehlivanova <mpehlivanova91@gmail.com>
2024-04-18 14:08:06 +0300

    feat: implement navigation

--- src/layout/constants.tsx
+++ src/layout/constants.tsx
@@ -1,101 +1,50 @@
-import React from 'react';
-import {
-  HomePage,
-  FavoritePage,
-  HistoryPage,
-  LoginPage,
-  PageNotFound,
-  FriendsPage,
-  GroupsPage,
-  MessagePage,
-  NotificationsPage,
-  ProfilePage,
-  VideoPage,
-  UserPage
-} from '../pages'
 import { Pages, PERMISSIONS } from '../enum';
 
 export const routeConfiguration = {
   favorite: {
     id: Pages.favorite,
-    isRequireAuth: true,
-    url: `/${Pages.favorite}`,
     permission: [PERMISSIONS.READ],
-    component: <FavoritePage />,
   },
   friends: {
     id: Pages.friends,
-    isRequireAuth: true,
-    url: `/${Pages.friends}`,
     permission: [PERMISSIONS.READ],
-    component: <FriendsPage />,
   },
   groups: {
     id: Pages.groups,
-    isRequireAuth: true,
-    url: `/${Pages.groups}`,
     permission: [PERMISSIONS.READ],
-    component: <GroupsPage />,
   },
   home: {
     id: Pages.home,
-    isRequireAuth: true,
-    url: `/${Pages.home}`,
     permission: [PERMISSIONS.READ],
-    component: <HomePage />,
   },
   login: {
     id: Pages.login,
-    isRequireAuth: false,
-    url: '/',
-    component: <LoginPage />,
   },
   message: {
     id: Pages.message,
-    isRequireAuth: true,
-    url: `/${Pages.message}`,
     permission: [PERMISSIONS.READ, PERMISSIONS.CREATE],
-    component: <MessagePage />,
   },
   notifications: {
     id: Pages.notifications,
-    isRequireAuth: true,
-    url: `/${Pages.notifications}`,
     permission: [PERMISSIONS.READ, PERMISSIONS.CREATE],
-    component: <NotificationsPage />,
   },
   pageNotFound: {
     id: Pages.pageNotFound,
-    isRequireAuth: false,
-    url: '*',
-    component: <PageNotFound />,
   },
   profile: {
     id: Pages.profile,
-    isRequireAuth: true,
-    url: `/${Pages.profile}`,
     permission: [PERMISSIONS.READ, PERMISSIONS.DELETE],
-    component: <ProfilePage />,
   },
   history: {
     id: Pages.history,
-    isRequireAuth: true,
-    url: `/${Pages.history}`,
     permission: [PERMISSIONS.READ, PERMISSIONS.DELETE],
-    component: <HistoryPage />,
   },
   user: {
     id: Pages.user,
-    isRequireAuth: true,
-    url: `/${Pages.user}`,
     permission: [PERMISSIONS.READ, PERMISSIONS.UPDATE],
-    component: <UserPage />,
   },
   video: {
     id: Pages.video,
-    isRequireAuth: true,
-    url: `/${Pages.video}`,
     permission: [PERMISSIONS.READ, PERMISSIONS.UPDATE],
-    component: <VideoPage />,
   },
 };
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,30 +1,14 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 import { Grid } from '@mui/material';
 import { useUserContext } from '../hooks/useUserContext';
-import { routeConfiguration } from '../layout/constants';
-import { RestrictedAccess } from '../auth/restricted-access'
-import { RouteConfigurationType } from '../types';
-import { Pages } from '../enum';
 
 const HomePage = () => {
   const { user } = useUserContext();
-  const permissionsUser: any = user?.permissions;
 
   return (
     <Grid container spacing={2}>
+      <Grid item xs={12}><h1>Home page</h1> </Grid>
       <Grid item xs={12}><h1>Welcome, {user?.name || user?.email} !</h1></Grid>
-      <Grid item container spacing={2} flexDirection="row">
-        {Object.values(routeConfiguration).map(({ isRequireAuth, url, id, permission }: RouteConfigurationType) => {
-          if (isRequireAuth && id !== Pages.home) {
-            return (
-              <RestrictedAccess permissionsPage={permission} permissionsUser={permissionsUser}>
-                <Grid item key={id}><Link to={`${url}`}>{id} page</Link></Grid>
-              </RestrictedAccess>
-            )
-          }
-        })}
-      </Grid>
     </Grid>
   );
 }
