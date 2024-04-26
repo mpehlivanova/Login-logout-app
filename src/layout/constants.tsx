d73b55a598be628dd21b46d509b810fa51c4a6e9
Mariela Pehlivanova <mpehlivanova91@gmail.com>
2024-04-26 17:49:22 +0300

    feat: add navigation bar

--- src/layout/constants.tsx
+++ src/layout/constants.tsx
@@ -1,50 +1,75 @@
+import React from 'react';
+import { Favorite, Group, History, Home, Message, Notifications, People, Person, Style } from '@mui/icons-material';
 import { Pages, PERMISSIONS } from '../enum';
 
+
 export const routeConfiguration = {
-  favorite: {
-    id: Pages.favorite,
-    permission: [PERMISSIONS.READ],
-  },
-  friends: {
-    id: Pages.friends,
-    permission: [PERMISSIONS.READ],
-  },
-  groups: {
-    id: Pages.groups,
-    permission: [PERMISSIONS.READ],
-  },
   home: {
     id: Pages.home,
+    link: Pages.home,
     permission: [PERMISSIONS.READ],
-  },
-  login: {
-    id: Pages.login,
-  },
-  message: {
-    id: Pages.message,
-    permission: [PERMISSIONS.READ, PERMISSIONS.CREATE],
-  },
-  notifications: {
-    id: Pages.notifications,
-    permission: [PERMISSIONS.READ, PERMISSIONS.CREATE],
-  },
-  pageNotFound: {
-    id: Pages.pageNotFound,
+    icon: <Home />
   },
   profile: {
     id: Pages.profile,
-    permission: [PERMISSIONS.READ, PERMISSIONS.DELETE],
+    link: Pages.profile,
+    permission: [PERMISSIONS.READ],
+    icon: <Person />
+  },
+  favorite: {
+    id: Pages.favorite,
+    link: Pages.favorite,
+    permission: [PERMISSIONS.READ],
+    icon: <Favorite />
+  },
+  friends: {
+    id: Pages.friends,
+    link: Pages.friends,
+    permission: [PERMISSIONS.READ],
+    icon: <People />
+  },
+  groups: {
+    id: Pages.groups,
+    link: Pages.groups,
+    permission: [PERMISSIONS.READ],
+    icon: <Group />
+  },
+  login: {
+    id: Pages.login,
+    link: Pages.login,
+  },
+  message: {
+    id: Pages.message,
+    link: Pages.message,
+    permission: [PERMISSIONS.READ, PERMISSIONS.CREATE],
+    icon: <Message />
+  },
+  notifications: {
+    id: Pages.notifications,
+    link: Pages.notifications,
+    permission: [PERMISSIONS.READ, PERMISSIONS.CREATE],
+    icon: <Notifications />
+  },
+  pageNotFound: {
+    id: Pages.pageNotFound,
+    link: Pages.pageNotFound,
   },
   history: {
     id: Pages.history,
+    link: Pages.history,
     permission: [PERMISSIONS.READ, PERMISSIONS.DELETE],
+    icon: <History />
   },
   user: {
     id: Pages.user,
+    link: Pages.user,
     permission: [PERMISSIONS.READ, PERMISSIONS.UPDATE],
+    icon: <Person />
   },
-  video: {
+  style: {
     id: Pages.style,
+    link: Pages.style,
     permission: [PERMISSIONS.READ],
+    icon: <Style />
   },
 };
--- src/pages/notifications.tsx
+++ src/pages/notifications.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 import { Grid } from '@mui/material';
-import { Pages } from '../enum';
 
 const NotificationsPage = () => {
   return (
     <Grid container>
       <Grid item><h1>Notifications page</h1></Grid>
-      <Grid item xs={12}> <Link to={`/${Pages.home}`}>back</Link></Grid>
     </Grid>
   );
 }
